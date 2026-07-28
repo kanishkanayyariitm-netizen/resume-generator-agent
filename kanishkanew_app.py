2ceee79c7388ae2f3d740996f5b56b2ceac88506
kanishkanayyariitm-netizen <kanishkanayyar.iitm@gmail.com>
2026-07-28 15:22:02 +0530

    Update kanishkanew_app.py

--- kanishkanew_app.py
+++ kanishkanew_app.py
@@ -12,6 +12,10 @@ from langchain.messages import SystemMessage, HumanMessage
 from langchain.agents import create_agent
 import tempfile
 
+
+# streamlit is web based python frame work
+st.title("AI RESUME MAKER & JOB APPLY AGENT")
+st.image("https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2F217gvesbwwjbio4tgoji.png", width=300)
 # =========================FRONTEND==================
 st.title("AI RESUME GENERATOR")
 
